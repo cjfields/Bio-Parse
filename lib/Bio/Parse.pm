5288c4d3be24f674ae9bde7f0905a3e527c83429
Chris Fields <cjfields@bioperl.org>
2011-10-10 22:14:57 -0500

    finish initial interface, will likely change but the docs are up

--- lib/Bio/Parse.pm
+++ lib/Bio/Parse.pm
@@ -359,6 +359,36 @@ recommended (can be used for indexers)
 Length of the B<raw data> in the stream. Not required but highly recommended
 (can be used for indexers)
 
+=back
+
+=head2 What Bio::Parse is not
+
+The following are NOT primary goals for Bio::Parse.
+
+=over 3
+
+=item * Validation
+
+Making this a validation tool. This will handle very low-level parsing of data
+at a very generic level, nothing more.
+
+=item * Output
+
+Having the tools write output. This is a *parser*, not a *writer*.  One can
+certainly use data from Bio::Parse to do this, however.
+
+=item * Class-dependency
+
+This will remain independent of BioPerl or any other specific biological data
+class/toolkit.
+
+=item * Dependencies
+
+Parsers that may require additional dependencies above and beyond what
+Bio::Parse requires should be distributed separately. Bio::Parse is a pluggable
+system, it's fairly easy to add new formats as needed and release them to CPAN
+(along with dependency requirements beyond Bio::Parse itself).
+
 =head1 SUBROUTINES/METHODS
 
 <TODO>
@@ -449,58 +479,3 @@ For Perl code that is often just:
 This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself. See L<perlartistic>.
 
-Pre-POD ramblings:
-
-Initial goal: parse data into specific data structures that are somewhat in
-relation to common Bio* classes, but aren't forcing one into using BioPerl or
-other object systems. Data is passed as a stream of simple data structures with
-as little nesting as possible and with very little to no coercing of data (that
-is left up to any downstream data handlers).  This will decouple parsing of data
-from specific object creation.
-
-There is some initial work along these lines within BioPerl itself, namely
-Bio::SeqIO::gbdriver/swisshandler/emblhandler/fastq and Bio::AlignIO::stockholm,
-but they all have specific somewhat hacky elements due to forcing them into the
-BioPerl class system. Because I am experimenting with a Moose-based system, it
-seemed like a good time to work on decoupling the parsing and object creation
-aspects in BioPerl, make the parsing perhaps a little less reliant on the
-overall BioPerl class structure. And, sometimes I really just want to get
-through data very quickly.
-
-Data structures will be derived from those. For instance, data structures for
-sequence formats will revolve around the sequence, sequence features, and report
-annotation. A lot of prior art exists in this field, much of which define very
-specific data types for describing pieces of biological information. I
-anticipate drawing from such sources like Chado, GusDB, the Bio* classes, and so
-on to derive these structures.
-
-NOTAGOAL:
-
-  * Making this a validation tool.  This will handle very low-level
-    parsing of data at a very generic level, nothing more.
-  * Having the tools write output (may change, but it will be very simple). This
-    is a *parser*, not a *writer*
-  * Forcing this into one class system or another (this will remain independent
-    of BioPerl)
-  * Adding excessive dependencies beyond what is described.  This doesn't mean
-    that something like an XML-based system will not be allowed, but I would
-    like for those to be distributed independently of these modules. Note, this
-    also allows one to write up other means of parsing (such as C-based parsers)
-    independently.
-
-SIMPLE DATA FORMAT
-
-# data relevant
-MODE = the data type being passed (as well as it can be defined)
-DATA = the raw data string for this mode (minus any mode-relative information)
-META = a data structure (generally a hash reference) containing possibly
-relevant information.  Redundant combined with the data above, but easier to
-digest :)
-
-# for those interested in indexing a file for whatever reason (this is NYI):
-START = start of the data chunk in the data stream
-LENGTH = length of the data chunk
-
-# Should we have different levels of a parse (e.g. if we are only interested
-in indexing on a per-record basis, maybe just parse at the top-level? That might
-be possible...
